6e8c5befb04e97b886b5ebee2aa993c8274222c5
alledarw <alexander.darwiche@gmail.com>
2024-02-07 19:03:49 +0100

    Added logic to pass choosen service to staff.html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import (Flask, render_template)
 import json
-
+from backend.service import service
 from backend.backend import Backend
 from frontend.http_request import http_request
 import frontend.utility as utility
@@ -26,16 +26,23 @@ def home():
     return render_template("home.html", service_items=service_items)
 
 
-@app.route('/staff', methods=['GET'], endpoint="staff")
-def staff():
-    staff_items = [
-        {"staff_code": "S001", "staff_name": "John Doe", "image": "john.jpg"},
-        {"staff_code": "S002", "staff_name": "Alice Smith", "image": "alice.jpg"},
-        {"staff_code": "S003", "staff_name": "Bob Johnson", "image": "bob.jpg"}
-    ]
+@app.route('/staff/<service_code>', methods=['GET'])
+def staff(service_code):
+    services = service.get_all_services()
+    selected_service = None
+    for service_item in services:
+        if service_code == service_item.get("service_id"):
+            print(service_item)
+            selected_service = service_item
+            break
+
+    staff_items = []
+    for service_item in services:
+        staff_list = service_item.get("staffs", [])
+        staff_items.extend(staff_list)
 
     print(staff_items)
-    return render_template("staff.html", staff_items=staff_items)
+    return render_template("staff.html", staff_items=staff_items, selected_service=selected_service)
 
 
 # -------- ERROR HANDLER  ------------
@@ -45,4 +52,4 @@ app.register_error_handler(401, utility.page_401)
 
 # For testing in Pycharm IDE
 #if __name__ == "__main__":
-#    app.run(debug=True)
+#   app.run(debug=True)
