447a366673908690fa8eea1b2d9c48ad2bb39edd
iamWGO <gottlieb@gmail.com>
2024-02-15 09:15:03 +0100

    ADDappointment_scheduling TABLE into table.sql

---
+++ Database/query.sql
@@ -0,0 +1,11 @@
+SELECT JSON_BUILD_OBJECT(
+                            'id', id,
+                            'srs_id', srs_id,
+                            'booking_code', booking_code,
+                            'start_at', start_at,
+                            'end_at', end_at,
+                            'booked_date', booked_date,
+                            'email', email,
+                            'is_booked', is_booked
+                            ) AS json_data
+                            FROM appointment_scheduling where srs_id IN (2, 5, 6, 9, 12, 15);
--- Database/tables.sql
+++ Database/tables.sql
@@ -1,7 +1,11 @@
-DROP TABLE "staff_relate_service";
-DROP TABLE "service";
-DROP TABLE "staff";
+DROP TABLE IF EXISTS "staff_relate_service";
+DROP TABLE IF EXISTS "service";
+DROP TABLE IF EXISTS "staff";
+DROP TABLE IF EXISTS "client_profile_relate_schedul";
+DROP TABLE IF EXISTS "client_profile";
+DROP TABLE IF EXISTS "appointment_scheduling";
 
+-- Sprint 1
 -- Create the service table
 CREATE TABLE "service" (
   "id" SERIAL PRIMARY KEY,
@@ -12,7 +16,7 @@ CREATE TABLE "service" (
   "price" INT,
   "time_in_minutes" INT,
   "is_active" BOOLEAN,
-  "updated" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated" TIMESTAMP,
   "created" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -24,7 +28,7 @@ CREATE TABLE "staff" (
   "staff_name" VARCHAR(255) NOT NULL,
   "image" VARCHAR(255),
   "description" VARCHAR(50) NOT NULL,
-  "updated" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated" TIMESTAMP,
   "created" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -36,6 +40,41 @@ CREATE TABLE "staff_relate_service" (
   UNIQUE (service_id, staff_id),
   FOREIGN KEY (service_id)  REFERENCES service(id),
   FOREIGN KEY (staff_id)  REFERENCES staff(id)
+);
+
+
+-- Sprint 2
+CREATE TABLE "appointment_scheduling" (
+  "id" SERIAL PRIMARY KEY,
+  "srs_id" INT,
+  "booking_code" VARCHAR(20),
+  "start_at" VARCHAR(5),
+  "end_at" VARCHAR(5),
+  "email" VARCHAR(200),
+  "booked_date" DATE,
+  "is_booked" bool,
+  "created" TIMESTAMP,
+  "updated" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Sprint 3
+CREATE TABLE "client_profile" (
+  "id" SERIAL PRIMARY KEY,
+  "email" VARCHAR(100),
+  "password" VARCHAR(50),
+  "first_name" VARCHAR(100),
+  "last_name" VARCHAR(100),
+  "phone" VARCHAR(20),
+  "profile_image" VARCHAR(255),
+  "created" datetime,
+  "updated" datetime
+);
+ 
+CREATE TABLE "client_profile_relate_schedul" (
+  "appointment_id" INT,
+  "client_id" INT,
+  FOREIGN KEY (appointment_id)  REFERENCES appointment_scheduling(id)
+  FOREIGN KEY (client_id)  REFERENCES client_profile(id)
 );
 
 
